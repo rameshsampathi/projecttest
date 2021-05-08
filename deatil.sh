f42c487b3487a0f5a8c64821a7ccef09ba46fcbb
root <root@ip-10-20-1-84.ec2.internal>
2021-05-08 04:39:39 +0000

    change the this script

--- deatil.sh
+++ deatil.sh
@@ -1,5 +1,12 @@
 #! /bin/bash 
 echo "this script will provide information about the local system"
-echo $(hostname)
-echo $(whoami)
-echo $(id -un)
+echo
+#GIVES HOSTNAME
+echo $(hostname) 
+echo
+#GIVES USER EXECUTED THIS SCRIPT
+echo $(whoami) 
+echo
+#GIVES USER IN THE SYSTEM
+USERS=$(cat /etc/passwd | cut -d ":" -f 1)
+echo $USERS
